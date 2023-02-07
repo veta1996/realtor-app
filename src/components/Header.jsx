bafa058f538992c7fbff68864084f6162aa2f9ff
veta_plotnikova <veta.plotnikova@gmail.com>
2023-02-06 19:38:42 -0800

    Completed the header component dynamic Sign in/Profile

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import React, { useEffect, useState } from 'react'
 import { useLocation, useNavigate } from 'react-router'
 
 const Header = () => {
     const location = useLocation()
-    const navigate = useNavigate()
-
+    const navigate = useNavigate();
+    const auth = getAuth();
+    useEffect(() => {
+        onAuthStateChanged(auth, (user) => {
+          if(user){
+            setPageState('Profile')
+          } else {
+            setPageState('Sign in')
+          }
+        })
+    }, [auth])
+    const [pageState, setPageState] = useState('Sign in')
     const pathMatchRoute = (route) => {
         if(route === location.pathname){
             return true;
@@ -28,8 +39,8 @@ const Header = () => {
                 border-b-transparent ${pathMatchRoute('/offers') && 'text-black border-b-red-500'}`}
                 onClick={() => navigate('/offers')}>Offers</li>
                 <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px]
-                border-b-transparent ${pathMatchRoute('/sign-in') && 'text-black border-b-red-500'}`}
-                onClick={() => navigate('/sign-in')}>Sign in</li>
+                border-b-transparent ${(pathMatchRoute('/sign-in') || pathMatchRoute('/profile')) && 'text-black border-b-red-500'}`}
+                onClick={() => navigate('/profile')}>{pageState}</li>
             </ul>
         </div>
       </header>
