e937640c70e53c89639db42e7bfe80476d70c11f
veta_plotnikova <veta.plotnikova@gmail.com>
2023-02-16 15:50:10 -0800

    added ui for ListingItem component

--- src/components/ListingItem.jsx
+++ src/components/ListingItem.jsx
@@ -1,12 +1,46 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import Moment from 'react-moment'
+import {MdLocationOn} from 'react-icons/md'
 
 const ListingItem = ({listing, key, id}) => {
     console.log(listing)
+    
   return (
-    <div>
-        <p>{listing.address}</p>
-        <p>{listing.regularPrice}</p>
-        </div>
+    <li className='relative flex flex-col justify-between items-center shadow-md hover:shadow-xl
+    rounded-md overflow-x-auto transition-shadow duration-150 m-[10px]'>
+        <Link to={`/category/${listing.type}/${id}`} className="contents">
+            <img src={listing.imgUrls[0]} alt={listing.description}
+            className='h-[170px] w-full object-cover hover:scale-105 transition-scale 
+            duration-200 ease-in'
+            loading='lazy'/>
+            <Moment fromNow className='absolute top-2 left-2 bg-[#3377cc] text-white
+            text-xs font-semibold rounded-md px-2 py-1 shadow-lg'>{listing.timestamp?.toDate()}</Moment>
+            <div className='w-full p-[10px]'>
+                <div className='flex items-center space-x-1'>
+                    <MdLocationOn className='h-4 w-4 text-green-600' />
+                    <p className='font-semibold text-sm mb-[2px] text-gray-600 truncate'>{listing.address}</p>
+                </div>
+                    <p className='font-semibold m-0 text-lg truncate'>{listing.name}</p>
+                    <p className='text-[#457b9d] mt-2 font-semibold'>${listing.offer ? listing.discountPrice
+                    .toString()
+                    .replace(/\B(?=(\d{3})+(?!\d))/g, ",") : 
+                    listing.regularPrice
+                    .toString()
+                  .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+                  {listing.type === 'rent' && ' / month'}
+                  </p>
+                  <div className='flex items-center mt-[10px] space-x-3'>
+                    <div className='flex items-center space-x-1'>
+                        <p className='font-bold text-sm'>{listing.bedrooms > 1 ? `${listing.bedrooms} Beds` : `${listing.bedrooms} Bed`}</p>
+                    </div>
+                    <div className='flex items-center space-x-1'>
+                        <p className='font-bold text-sm'>{listing.bathrooms > 1 ? `${listing.bathrooms} Baths` : `${listing.bathrooms} Bath`}</p>
+                    </div>
+                  </div>
+            </div>
+        </Link>
+    </li>
   )
 }
 
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -119,12 +119,14 @@ export default function Profile() {
           </button>
       </div>
     </section>
-    
+
     <div className='max-w-6xl px-3 mt-6 mx-auto'>
       {!loading && listings.length > 1 && (
         <>
-        <h2 className='text-2xl text-center font-semibold'>My Listings</h2>
-        <ul>{listings.map((listing)=> (
+        <h2 className='text-2xl text-center font-semibold mb-6'>My Listings</h2>
+        <ul className='sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4
+        2xl:grid-cols-5 mt-6 mb-6'>
+          {listings.map((listing)=> (
           <ListingItem listing={listing.data} key={listing.id} id={listing.id}/>
         ))}</ul>
         </>
